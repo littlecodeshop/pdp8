d0d3eb0daf0ccbaee2477edb79a3da45b023f6c6
Richard Ribier <coders@littlecodeshop.com>
2016-04-13 13:57:20 +0000

    non blocking key input ca marche

--- main.c
+++ main.c
@@ -5,6 +5,48 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/select.h>
+#include <termios.h>
+
+struct termios orig_termios;
+void reset_terminal_mode()
+{
+    tcsetattr(0, TCSANOW, &orig_termios);
+}
+
+void set_conio_terminal_mode()
+{
+    struct termios new_termios;
+
+    /* take two copies - one for now, one for later */
+    tcgetattr(0, &orig_termios);
+    memcpy(&new_termios, &orig_termios, sizeof(new_termios));
+
+    /* register cleanup handler, and set the new terminal mode */
+    atexit(reset_terminal_mode);
+    cfmakeraw(&new_termios);
+    tcsetattr(0, TCSANOW, &new_termios);
+}
+
+int kbhit()
+{
+    struct timeval tv = { 0L, 0L };
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(0, &fds);
+    return select(1, &fds, NULL, NULL, &tv);
+}
+
+int getch()
+{
+    int r;
+    unsigned char c;
+    if ((r = read(0, &c, sizeof(c))) < 0) {
+        return r;
+    } else {
+        return c;
+    }
+}
 
 typedef enum {
     AND = 0,
@@ -822,6 +864,8 @@ int main(int argc, char ** argv){
     loadAddress();
     cpu.SR = 0000;
     int trace=0;
+    
+    set_conio_terminal_mode();
 
     while(1){ 
         if(interrupt_countdown > 0){
@@ -833,6 +877,11 @@ int main(int argc, char ** argv){
         
         }
         singleInstruction();
+        if(kbhit()){
+            char c = getch();
+            //printf("=>>>>>%c\n",c);
+            keyboard_input(c);
+        }
     }
 
 }
